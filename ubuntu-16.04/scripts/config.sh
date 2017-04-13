c73905f5734cf2f4a4e75de09584ff1bced1b5e0
Michael Pozhidaev <msp@altlinux.ru>
2017-04-13 20:25:46 +0700

    ubuntu-16.04:proper ISO image information

--- ubuntu-16.04/scripts/config.sh
+++ ubuntu-16.04/scripts/config.sh
@@ -1,4 +1,12 @@
 
+
+HMRISO_ARCH="$(uname -m)"
+HMRISO_DATE="$(date +%Y%m%d)"
+HMRISO_USER=homeros
+
 LWRISO_NAMESERVER=192.168.1.1
 LWRISO_LANG=ru
-HMRISO_USER=homeros
+
+if [ "$HMRISO_ARCH" == x86_64 ]; then
+    HMRISO_ARCH=amd64
+fi
