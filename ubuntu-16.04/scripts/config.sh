df0230f3af47815ca44e2d6f439d7f8687258837
Michael Pozhidaev <msp@altlinux.ru>
2017-04-14 22:49:00 +0700

    ubuntu-16.04:fixing ISO info

--- ubuntu-16.04/scripts/config.sh
+++ ubuntu-16.04/scripts/config.sh
@@ -1,12 +1,8 @@
 
 
-HMRISO_ARCH="$(uname -m)"
+HMRISO_ARCH=amd64
 HMRISO_DATE="$(date +%Y%m%d)"
 HMRISO_USER=homeros
 
 LWRISO_NAMESERVER=192.168.1.1
 LWRISO_LANG=ru
-
-if [ "$HMRISO_ARCH" == x86_64 ]; then
-    HMRISO_ARCH=amd64
-fi
