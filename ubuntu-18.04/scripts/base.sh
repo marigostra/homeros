7175750efdbe9eaff1d82046d9909916ce60f97b
Michael Pozhidaev <msp@luwrain.org>
2019-08-03 08:45:47 +0700

    Adding 18.04 as a copy of 16.04

---
+++ ubuntu-18.04/scripts/base.sh
@@ -0,0 +1,23 @@
+
+export LWRISO_ROOT=/iso/chroot
+export HMRISO_ROOT=/iso/chroot
+
+in-chroot()
+{
+    chroot ./chroot "$@"
+}
+
+chroot-run()
+{
+    chroot ./chroot "$@"
+}
+
+install-pkg()
+{
+chroot-run apt-get -y --force-yes install $@
+}
+
+remove-pkg()
+{
+chroot-run apt-get -y remove $@
+}
