8dacc9a9125f945027c72508378bd028a8bcc9c9
Michael Pozhidaev <msp@luwrain.org>
2021-07-12 14:51:33 +0700

    20.04: Removing config.sh

--- ubuntu-20.04/scripts/base.sh
+++ ubuntu-20.04/scripts/base.sh
@@ -1,11 +1,14 @@
 
-export LWRISO_ROOT=/iso/chroot
-export HMRISO_ROOT=/iso/chroot
 
-in-chroot()
-{
-    chroot ./chroot "$@"
-}
+
+HMRISO_ARCH=amd64
+HMRISO_DATE="$(date +%Y%m%d)"
+HMRISO_USER=homeros
+HMRISO_KERNEL=4.15.0-29-generic
+LWRISO_NAMESERVER=8.8.8.8
+LWRISO_LANG=ru
+
+export HMRISO_ROOT=/iso/chroot
 
 chroot-run()
 {
@@ -14,7 +17,7 @@ chroot-run()
 
 install-pkg()
 {
-chroot-run apt-get -y --force-yes install $@
+chroot-run apt-get -y install $@
 }
 
 remove-pkg()
