2ece845a324a3add0a04eebefee1e66717fe7248
Michael Pozhidaev <msp@luwrain.org>
2021-11-20 03:35:00 +0700

    Creating 21.10

---
+++ ubuntu-21.10/scripts/base.sh
@@ -0,0 +1,29 @@
+
+HMRISO_ARCH=amd64
+HMRISO_DATE="$(date +%Y%m%d)"
+HMRISO_USER=homeros
+HMRISO_NAMESERVER=8.8.8.8
+HMRISO_LANG=ru
+
+export HMRISO_ROOT=/iso/chroot
+
+chroot-run()
+{
+    chroot ./chroot "$@"
+}
+
+install-pkg()
+{
+chroot-run apt-get --yes install $@
+}
+
+remove-pkg()
+{
+chroot-run apt-get --yes remove $@
+}
+
+remove-pkg-prefix()
+{
+    remove-pkg $(apt-cache search "$1" | grep "^$1" | cut -f1 -d' ')
+}
+
