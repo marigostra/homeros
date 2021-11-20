52dfaa53cebae6e4b41d47cd30d3b2754604c066
Michael Pozhidaev <msp@luwrain.org>
2021-11-20 16:04:00 +0700

    21.10: Bootable image

--- ubuntu-21.10/scripts/base.sh
+++ ubuntu-21.10/scripts/base.sh
@@ -4,6 +4,7 @@ HMRISO_DATE="$(date +%Y%m%d)"
 HMRISO_USER=homeros
 HMRISO_NAMESERVER=8.8.8.8
 HMRISO_LANG=ru
+HMRISO_BOOT_VER=21.10
 
 export HMRISO_ROOT=/iso/chroot
 
@@ -21,6 +22,12 @@ remove-pkg()
 {
 chroot-run apt-get --yes remove $@
 }
+
+rm-pkg()
+{
+chroot-run apt-get --yes remove --ignore-missing --no-download $@
+}
+
 
 remove-pkg-prefix()
 {
